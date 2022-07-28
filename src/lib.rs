38965a2d9c30a802de2e5403be138c5f06505f96
Carl Raffaele <samfisher.50@gmail.com>
2022-07-27 22:16:42 -0400

    Tests and skeleton code

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,22 @@
-use eframe::egui::TextBuffer;
 use std::cmp::min;
-use std::ops::Range;
-use std::str::{from_utf8, from_utf8_unchecked};
+use std::iter::FusedIterator;
+use std::str::{Chars, from_utf8, from_utf8_unchecked};
 
+#[derive(Copy, Clone, Debug)]
 struct StRingBuffer<const size: usize> {
     data: [u8; size],
     head: usize,
     tail: usize,
 }
 
+#[derive(Clone, Debug)]
 struct HeapStRingBuffer {
     data: Box<[u8]>,
     head: usize,
     tail: usize,
 }
 
-trait StringBuffer: TextBuffer {
+trait StringBuffer {
     /// Adds a char to the buffer. Overwrites the start if the buffer is full.
     fn push_char(&mut self, c: char);
 
@@ -28,46 +29,24 @@ trait StringBuffer: TextBuffer {
     /// reference will be an empty &str.
     fn as_slices(&self) -> (&str, &str);
 
-    /// Moves the head of the ring to the start of the buffer. Required to represent the entire
-    /// buffer as a single &str
+    /// Copies data as required to make the head the start of the buffer. Required to represent the
+    /// entire buffer as a single &str.
     fn align(&mut self);
 
     /// Moves the head of the buffer near the specified index. Will move to an index >= the given
     /// index ensuring that the head lies on a valid UTF-8 boundary.
     fn move_head(&mut self, index: usize) -> usize;
-}
 
-// impl<const size: usize> TextBuffer for StRingBuffer<size> {
-//     fn is_mutable(&self) -> bool {
-//         true
-//     }
-//
-//     fn char_range(&self, char_range: Range<usize>) -> &str {
-//         todo!()
-//     }
-//
-//     fn byte_index_from_char_index(&self, char_index: usize) -> usize {
-//         todo!()
-//     }
-//
-//     fn insert_text(&mut self, text: &str, char_index: usize) -> usize {
-//         todo!()
-//     }
-//
-//     fn delete_char_range(&mut self, char_range: Range<usize>) {
-//         todo!()
-//     }
-//
-//     fn clear(&mut self) {
-//         self.head = 0;
-//         self.tail = 0;
-//     }
-//
-//     fn replace(&mut self, text: &str) {
-//         self.clear();
-//         self.push_str(text);
-//     }
-// }
+    /// Returns the length of this buffer, in bytes, not chars or graphemes
+    fn len(&self) -> usize;
+
+    fn capacity(&self) -> usize;
+
+    fn chars(&self) -> std::iter::Chain<Chars<'_>, Chars<'_>> {
+        let (front, back) = self.as_slices();
+        front.chars().chain(back.chars())
+    }
+}
 
 impl<const size: usize> AsRef<str> for StRingBuffer<size> {
     fn as_ref(&self) -> &str {
@@ -110,14 +89,197 @@ impl<const size: usize> StringBuffer for StRingBuffer<size> {
     fn move_head(&mut self, index: usize) -> usize {
         todo!()
     }
+
+    fn len(&self) -> usize {
+        todo!()
+    }
+
+    fn capacity(&self) -> usize {
+        self.data.len()
+    }
+}
+
+impl<const size: usize> StRingBuffer<size> {
+    const fn new() -> Self {
+        Self{
+            data: [0; size],
+            head: 0,
+            tail: 0
+        }
+    }
+}
+
+impl StringBuffer for HeapStRingBuffer {
+    fn push_char(&mut self, c: char) {
+        todo!()
+    }
+
+    fn push_str(&mut self, s: &str) {
+        todo!()
+    }
+
+    fn as_slices(&self) -> (&str, &str) {
+        todo!()
+    }
+
+    fn align(&mut self) {
+        todo!()
+    }
+
+    fn move_head(&mut self, index: usize) -> usize {
+        todo!()
+    }
+
+    fn len(&self) -> usize {
+        todo!()
+    }
+
+    fn capacity(&self) -> usize {
+        self.data.len()
+    }
+}
+
+impl HeapStRingBuffer {
+    pub fn new(size: usize) -> Self {
+        HeapStRingBuffer{
+            data: vec![0; size].into_boxed_slice(),
+            head: 0,
+            tail: 0
+        }
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use crate::{HeapStRingBuffer, StRingBuffer, StringBuffer};
+
+    const SMALL_SIZE: usize = 5;
+    const SMALL_CONST: StRingBuffer<SMALL_SIZE> = StRingBuffer::new();
+
+    fn small_heap() -> HeapStRingBuffer{
+        HeapStRingBuffer::new(SMALL_SIZE)
+    }
+
+    fn basic_helper(test: &mut impl StringBuffer){
+        test.push_char('A');
+        assert_eq!(test.len(), 1);
+        assert_eq!(test.as_slices().0, "A");
+        assert_eq!(test.as_slices().1, "");
+        test.push_str("BCDE");
+        assert_eq!(test.len(), 5);
+        assert_eq!(test.as_slices().0, "ABCDE");
+        assert_eq!(test.as_slices().1, "");
+
+        assert_eq!(SMALL_SIZE, 5);
+        test.push_char('X');
+        assert_eq!(test.len(), 5);
+        assert_eq!(test.as_slices().0, "BCDE");
+        assert_eq!(test.as_slices().1, "X");
+        test.align();
+        assert_eq!(test.len(), 5);
+        assert_eq!(test.as_slices().0, "BCDEX");
+        assert_eq!(test.as_slices().1, "");
+    }
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn basic(){
+        basic_helper(&mut SMALL_CONST.clone());
+        basic_helper(&mut small_heap());
+    }
+
+    fn two_byte_helper(test: &mut impl StringBuffer) {
+        assert_eq!(test.capacity(), 3);
+        test.push_str("ABC");
+        //[^A, B, C*]
+        assert_eq!(test.len(), 3);
+        test.push_char('Ɵ'); //Latin Capital Letter O with Middle Tilde (0xC6 0x9F in UTF-8)
+        //[0xC6, 0x9F, *^C]
+        assert_eq!(test.len(), 3);
+        assert_eq!(test.as_slices().0, "C");
+        assert_eq!(test.as_slices().1, "Ɵ");
+        {
+            let iter = test.chars();
+            assert_eq!(test.next(), Some('C'));
+            assert_eq!(test.next(), Some('Ɵ'));
+            assert_eq!(test.next(), None);
+        }
+
+        test.push_str("XY");
+        //[Y*, _, ^X]
+        assert_eq!(test.len(), 2);
+        assert_eq!(test.as_slices().0, "X");
+        assert_eq!(test.as_slices().1, "Y");
+        {
+            let iter = test.chars();
+            assert_eq!(test.next(), Some('X'));
+            assert_eq!(test.next(), Some('Y'));
+            assert_eq!(test.next(), None);
+        }
+
+        //split on buffer end
+        test.push_char('Z');
+        //[Y, Z, *^X]
+        assert_eq!(test.len(), 3);
+        test.push_char('Ɵ');
+        //[^0xC6, 0x9F*, _]
+        assert_eq!(test.len(), 2);
+        assert_eq!(test.as_slices().0, "Ɵ");
+        assert_eq!(test.as_slices().1, "");
+        {
+            let iter = test.chars();
+            assert_eq!(test.next(), Some('Ɵ'));
+            assert_eq!(test.next(), None);
+        }
+
+        test.push_char('ƛ'); //Latin Small Letter Lambda with Stroke (UTF-8: 0xC6 0x9B)
+        //[^0xC6, 0x9B*, _]
+        assert_eq!(test.len(), 2);
+        assert_eq!(test.as_slices().0, "ƛ");
+        assert_eq!(test.as_slices().1, "");
+        {
+            let iter = test.chars();
+            assert_eq!(test.next(), Some('ƛ'));
+            assert_eq!(test.next(), None);
+        }
+
+        //three bytes
+        test.push_char('Ꙃ'); //Cyrillic Capital Letter Dzelo (UTF-8: 0xEA 0x99 0x82)
+        //[^0xEA, 0x99, 0x8x*]
+        assert_eq!(test.len(), 1);
+        assert_eq!(test.as_slices().0, "Ꙃ");
+        assert_eq!(test.as_slices().1, "");
+        {
+            let iter = test.chars();
+            assert_eq!(test.next(), Some('Ꙃ'));
+            assert_eq!(test.next(), None);
+        }
+        test.push_char('A');
+        //[^A*, _, _]
+        assert_eq!(test.len(), 1);
+        assert_eq!(test.as_slices().0, "A");
+        assert_eq!(test.as_slices().1, "");
+        {
+            let iter = test.chars();
+            assert_eq!(test.next(), Some('A'));
+            assert_eq!(test.next(), None);
+        }
+
+        //four bytes (too big for buffer)
+        test.push_char('🦀'); //Crab Emoji (Ferris) (UTF-8: 0xF0 0x9F 0xA6 0x80)
+        //[^_*, _, _]
+        assert_eq!(test.len(), 0);
+        assert_eq!(test.as_slices().0, "");
+        assert_eq!(test.as_slices().1, "");
+        {
+            let iter = test.chars();
+            assert_eq!(test.next(), None);
+        }
+    }
+
+    #[test]
+    fn two_byte(){
+        let mut con = StRingBuffer::<3>::new();
+        two_byte_helper(&mut con);
+        two_byte_helper(&mut HeapStRingBuffer::new(3));
     }
 }
